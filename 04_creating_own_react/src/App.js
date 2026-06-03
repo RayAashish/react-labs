ff4712bceb9339d7260850d1ce0c0304998d0725
Aashish Kumar Ray <irealaashish@gmail.com>
2026-06-03 23:32:12 +0530

    Custom Basic  React

--- 04_creating_own_react/src/App.js
+++
@@ -1,5 +0,0 @@
-const child = "<h1> Hello Universe </h1>";
-function App(){
-    return child;
-}
-export default App
--- 04_creating_own_react/src/main.js
+++ 04_creating_own_react/src/main.js
@@ -1,5 +1,22 @@
-import App from "./App";
+function customRendrer(element, container){
+    const domElement = document.createElement(element.type);
+    domElement.innerHTML = element.children;
+    for (const prop in element.props){
+        if (prop === 'children') continue;
+        domElement.setAttribute(prop, element.props[prop]);
+    }
+    container.appendChild(domElement);
+}
 
-const root = document.getElementById("root");
+const reactElement = {
+    type : "a",
+    props : {
+        href : "https://github.com/RayAashish",
+        target : "_blank"
+    },
+    children : "CLICK TO VISIT GITHUB"
+}
 
-root.append(App())
+const container = document.getElementById("custom_root")
+
+customRendrer(reactElement, container);
