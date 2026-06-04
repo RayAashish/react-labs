7cf088c83c6a4cf0d15207c15f6d680dac8e58f5
Aashish Kumar Ray <irealaashish@gmail.com>
2026-06-04 13:20:51 +0530

    Internal working of React.createElement method

--- 03_dive_into_react/src/App.jsx
+++ 03_dive_into_react/src/App.jsx
@@ -1,11 +1,10 @@
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <p>Hello from App</p>
     </>
   )
 }
--- 03_dive_into_react/src/main.jsx
+++ 03_dive_into_react/src/main.jsx
@@ -1,9 +1,29 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// function MyApp(){
+//   return(
+//     <>
+//       <p>This is a functio inside the main function.</p>
+//     </>
+//   )
+// }
+
+const user = "\nAashish";
+
+//Internal working of react
+//This is the schema used by createElement method of React.
+const CustomFunction = React.createElement(
+  'a',
+  {
+    href : "https://ai.com",
+    target : "_blank"
+  },
+  "Click to Go AI.com",
+  user
+);
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  CustomFunction
 )
