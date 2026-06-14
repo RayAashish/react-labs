85c9a947fcc362136cbdb9610ecbd9ef3b601ef1
Aashish Kumar Ray <irealaashish@gmail.com>
2026-06-14 07:44:21 +0545

    React simple project :: Background color changer

--- 09_react_project_bgChanger/src/App.jsx
+++ 09_react_project_bgChanger/src/App.jsx
@@ -1,18 +1,18 @@
+import { useState } from 'react'
 import './App.css'
-import Button from './component/Button'
 
 function App() {
-  const colors = ["red", "blue", "lavandar", "brown"]
+  const [color, setColor] = useState("lavander")
   return (
-      <body>
-        <div className='grid col-end-4 gap-4'>
-          {
-            colors.map((color, i) => (
-              <Button  color={colors[i]} />
-            ))
-          }
+        <div className='h-screen w-screen px-2 py-2' style={{backgroundColor : color}}>
+          <div className='bg-blue-300 p-2 flex justify-around rounded-2xl'>
+            <button onClick={() => setColor("red")} style={{backgroundColor : "red"}} className='h-10 w-20 bg-red-600 rounded-xl hover:bg-red-900 hover:cursor-pointer'>Red</button>
+            <button onClick={() => setColor("green")} className='h-10 w-20 bg-green-600 rounded-xl hover:bg-green-900 hover:cursor-pointer'>Green</button>
+            <button onClick={() => setColor("blue")} className='h-10 w-20 bg-blue-600 rounded-xl hover:bg-blue-900 hover:cursor-pointer'>Blue</button>
+            <button onClick={() => setColor("yellow")} className='h-10 w-20 bg-yellow-600 rounded-xl hover:bg-yellow-900 hover:cursor-pointer'>Yellow</button>
+            <button onClick={() => setColor("orange")} className='h-10 w-20 bg-orange-600 rounded-xl hover:bg-orange-900 hover:cursor-pointer'>Orange</button>
+          </div>
         </div>
-      </body>
   )
 }
 
