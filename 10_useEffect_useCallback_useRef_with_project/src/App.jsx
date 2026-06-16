3366694ba4ea43b566d9844edf181d4b43bdd8a8
Aashish Kumar Ray <irealaashish@gmail.com>
2026-06-16 19:08:36 +0545

    useEffect, useCallback, useRef hooks

---
+++ 10_useEffect_useCallback_useRef_with_project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-amber-600'>
+        <p>Hello World</p>
+      </div>
+    </>
+  )
+}
+
+export default App
